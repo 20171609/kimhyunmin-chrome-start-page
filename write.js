a378e404c495fe23d62ead838ef20c437271ed74
Kimhyunmin <clw3wb@naver.com>
2020-11-03 07:11:17 +0900

    게시판 완성

--- write.js
+++ write.js
@@ -10,6 +10,7 @@ function writePost(title, username, content){
     };
 
     firebase.database().ref().child('posts').push(postData);
+    location.href = "Add_Board.html";
 }
 
 function resister(){
@@ -26,4 +27,42 @@ function resister(){
     document.getElementById('contents').value = "";
 
     hideModal();
+}
+
+function showTitle(){
+    const postRef = firebase.database().ref('/posts/').once('value', function(snapshot){
+        const postData = Object.entries(snapshot.val());
+        let tableContent = document.querySelector('#content');
+        let num = postData.length-1;
+
+        for(let i = postData.length-1; i >= 0; i--){
+            const[key, body] = postData[i];
+
+
+            tableContent.innerHTML += "<tr><td>"+body.now+ "</td><td class='title' onclick='showContent(" + num +")'>" + body.title + "</td><td>" + body.username+"</td></tr>";
+            num--;
+        }
+    });
+}
+
+function showContent(num){
+    let modal2 = document.querySelector('.modal2');
+    let title = document.querySelector('#need_title');
+    let name = document.querySelector('#need_name');
+    let content = document.querySelector('#need_content');
+    modal2.style.display='block';
+
+    const postRef = firebase.database().ref('/posts/').once('value', function(snapshot){
+        const postData = Object.entries(snapshot.val());
+
+        for(let i = 0; i < postData.length; i++){
+            if(i === num){
+                const[key, body] = postData[i];
+                title.innerText += body.title;
+                name.innerText += body.username;
+                content.innerText += body.content;
+            }
+
+        }
+    });
 }
